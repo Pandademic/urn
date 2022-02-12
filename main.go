562423e1a8214e9947cba9d1f606c53ad40cbc78
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-02-12 17:31:14 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt"
+	f"fmt"
 	s "strings"
 )
 func main() {
 	code := "PRINT hewo"
-	ss := strings.Fields(s)
+	ss := s.Fields(code)
 	keyword := ss[0]
 	if(keyword == "PRINT"){
 		arg := ss[1]
-		p = fmt.Println
-		p(arg)
+		f.Println(arg)
 	}
 }
 
